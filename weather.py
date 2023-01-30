f07b0dfa8a3af337e4646bdac28c7318923c5d3f
Julie-mg <62802039+Julie-mg@users.noreply.github.com>
2023-01-30 23:31:16 +0100

    ajout socket

--- weather.py
+++ weather.py
@@ -21,6 +21,7 @@ class WeatherSimulator(Process):
                 self.temperature = 40
             # update shared memory with current temperature
             self.shared_memory[i] = self.temperature
+            print(self.shared_memory[i])
 
             self.day.wait()
             print(f'day {i} off_________________________________________')
